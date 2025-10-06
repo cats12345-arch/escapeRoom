33a940aa23df03e43e28a8512986ba7ba713f5fb
CSE-MilesDF <mdf6@email.sc.edu>
2025-10-06 09:38:48 -0400

    mere puzzle branches to main

---
+++ escaperoon/CipherPuzzle.java
@@ -0,0 +1,17 @@
+public class CipherPuzzle extends Puzzle {
+     protected int caesarCiphers;
+     protected String anagram;
+     private String hint;
+
+     public void solvePuzzle(String input) {
+
+     }
+
+     public void displayHint() {
+
+     }
+
+     public String toString() {
+        return "Cipher Puzzle with cipher :" + caesarCiphers + ", anagram: " + anagram;
+     }
+}
