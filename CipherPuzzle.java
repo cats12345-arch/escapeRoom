542857b6a8380129fce85d25567c980abce81884
CSE-MilesDF <mdf6@email.sc.edu>
2025-10-08 09:24:52 -0400

    Cipbher Class Cipher Prototype (Issues #39)

--- CipherPuzzle.java
+++ CipherPuzzle.java
@@ -1,14 +1,36 @@
 public class CipherPuzzle extends Puzzle {
-     protected int caesarCiphers;
+     protected int caesarCiphers = 4;
      protected String anagram;
-     private String hint;
+     private String hint = " The letters are shifted forward by " + caesarCiphers + ".";
+
+
+     private String encrypt(String text, int shift) {
+      StringBuilder result = new StringBuilder();
+      for (char c : text.toUpperCase().toCharArray()) {
+         if (Character.isLetter(c)) {
+            char shifted = (char) ((c- 'A' + shift)% 26 + 'A');
+            result.append(shifted);
+         } else {
+            result.append(c);
+         }
+
+      }
+      return result.toString();
+     }
 
      public void solvePuzzle(String input) {
+      if (input == null) return;
+      if (input.equalsIgnoreCase(solution)) {
+         this.solved = true;
+         System.out.println("Thats Correct! You solved the cipher.");
+      } else {
+         System.out.println("Incorrect");
+      }
 
      }
 
      public void displayHint() {
-
+      System.out.println("Hint: " + hint);
      }
 
      public String toString() {
