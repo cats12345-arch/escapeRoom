6a43e72f1e4e93971c2862ceb3480bba784be8f2
Daniel Moss <danielpmoss123@gmail.com>
2025-10-26 18:44:59 -0400

    #52

--- escaperoon/src/main/java/module-info.java
+++ escaperoon/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
+    requires freetts;
     opens com.example to javafx.fxml;
     exports com.example;
 }
